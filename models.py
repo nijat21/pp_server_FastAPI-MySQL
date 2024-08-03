f07ff7dc7e8b8c263e81d9338051107f588fd517
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-08-03 15:07:55 +0100

    Skeleton of FastAPI backend with MySQL missing oauth

--- models.py
+++ models.py
@@ -4,4 +4,13 @@ from database import Base
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, )
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True)
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50), unique=True)
+    content = Column(String(100))
+    user_id = Column(Integer)
