ec8a97aa3a1d52882e0e1e21df0af966724f9eb2
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-08-08 00:01:34 +0100

    Connected to AWS RDS

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-URL_DATABASE = 'mysql+pymysql://root:jamesbondreturns@localhost:3306/PhoenixPagesDB'
+load_dotenv()
+
+URL_DATABASE = os.getenv("URL_DATABASE")
 
 engine = create_engine(URL_DATABASE)
 
